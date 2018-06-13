4a0d6f69e6cc48e0f3fd2e28bf4abd2d6cfb5e03
MaximNd <max0350@gmail.com>
2018-06-13 21:35:07 +0300

    sh file with game

---
+++ guessinggame.sh
@@ -0,0 +1,24 @@
+echo "Guess a number of files in current directory"
+
+# Correct answer
+count_files=$(ls -1 | wc -l)
+# User guess
+user_guess=-1
+
+function play_game {
+    while [[ $user_guess != $count_files ]]
+    do
+        read -p "Enter number: " user_guess
+        if [[ $user_guess < $count_files ]] 
+        then
+            echo "Higher"
+        elif [[ $user_guess > $count_files ]] 
+        then
+            echo "Lower"
+        fi
+    done
+}
+
+play_game
+
+echo "Correct!"
